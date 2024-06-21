71d6883b49e107bbf0ad96e316a15a7350f6996f
Torrey Smith <tas.smity@yahoo.com>
2024-06-21 14:43:41 -0700

    Minor adjustments to the navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,3 +1,5 @@
+// File 1: /components/layout/Navbar.tsx
+
 'use client';
 
 import React from 'react';
@@ -20,7 +22,7 @@ export default function Navbar({ page }: { page: string }) {
         return (
             <Link key={menu_class_name} href="/" className={`${menu_class_name === 'testimonials' || menu_class_name === 'portfolio' ? 'hidden md:flex' : ''}`}>
                 <div
-                    className={`h-full leading-[50px] font-bold cursor-pointer text-transparent bg-clip-text ${
+                    className={`h-full pb-1 font-bold cursor-pointer text-transparent bg-clip-text ${
                         is_selected
                             ? 'bg-gradient-to-r from-yellow-500 via-amber-600 to-yellow-500'
                             : 'bg-gradient-to-r from-yellow-400 via-amber-500 to-yellow-400 hover:from-yellow-500 hover:via-amber-600 hover:to-yellow-500'
@@ -33,26 +35,28 @@ export default function Navbar({ page }: { page: string }) {
         );
     });
 
-    const halfLength = Math.ceil(navbar.length / 2);
+    const halfLength = Math.floor(navbar.length / 2);
+    const leftNavbar = navbar.slice(0, halfLength);
+    const rightNavbar = navbar.slice(halfLength);
 
     return (
         <nav className="bg-neutral-900 p-0 flex flex-row h-[50px] w-full items-center justify-center">
             <div className="hidden md:flex flex-row space-x-4 items-center justify-end">
-                {navbar.slice(0, halfLength)}
+                {leftNavbar}
             </div>
-            <Link href="/">
-                <div className="hidden md:flex max-h-[50px] w-auto relative mx-4 cursor-pointer" onClick={() => handleSelectPage('home')}>
+            <Link href="/" className="hidden md:block mx-4 pb-1">
+                <div className="flex flex-row space-x-4 items-center justify-end">
                     <Image
                         src="/logo/CCS_logo_text.png"
                         alt="CCS Logo"
                         width={247}
                         height={88}
-                        className="object-contain max-h-full w-fit pt-2 pb-1"
+                        className="object-contain max-h-[48px] w-fit pt-2 pb-1"
                     />
                 </div>
             </Link>
-            <div className="hidden md:flex flex-row space-x-4 items-center justify-start ">
-                {navbar.slice(halfLength)}
+            <div className="hidden md:flex flex-row space-x-4 items-center justify-start">
+                {rightNavbar}
             </div>
             <div className="md:hidden flex flex-row space-x-4 items-center justify-center w-full">
                 {navbar}
