941222cb7328aac760bde5170cda9637f5b9a111
Torrey Smith <tas.smity@yahoo.com>
2024-06-21 15:30:24 -0700

    made testimonials more consistent accross screen sizes

--- app/_main_components/testimonials.tsx
+++ app/_main_components/testimonials.tsx
@@ -20,14 +20,14 @@ const testimonials = [
 
 export default function Testimonials() {
     return (
-        <div className='flex flex-col w-full space-y-4 h-full'>
+        <div className='flex flex-col w-full space-y-4 h-full justify-center items-center'>
             <div className="w-full h-auto flex items-center justify-center">
-                <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-secondary_light via-secondary_dark to-secondary_light text-center w-fit">
+                <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-secondary_light via-secondary_dark to-secondary_light text-center">
                     Testimonials
                 </h1>
             </div>
 
-            <div className="flex flex-row justify-center items-center space-x-4 p-4">
+            <div className="flex flex-row justify-center items-center space-x-4 px-4">
                 {testimonials.map((testimonial, index) => (
                     <div className="relative flex flex-col w-full h-full" key={index}>
                         <Image
@@ -35,9 +35,9 @@ export default function Testimonials() {
                             alt={testimonial.name}
                             width={testimonial.width}
                             height={testimonial.height}
-                            className="rounded-lg shadow-lg w-auto md:h-[40vh] m-auto"
+                            className="rounded-lg shadow-lg w-auto sm:h-[45vh] m-auto"
                         />
-                        <div className="md:absolute md:top-[75%] lg:top-[80%] h-fit bg-secondary_dark text-white p-4 rounded-lg shadow-lg">
+                        <div className="absolute top-[85%] sm:top-[75%] lg:top-[80%] h-fit bg-secondary_dark text-white p-4 rounded-lg shadow-lg">
                             <p className="text-md md:text-lg">{`"${testimonial.text}"`}</p>
                             <p className="mt-2 text-md font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-amber-500 to-yellow-400">
                                 {testimonial.name}
--- app/_main_components/testimonials_and_statistics.tsx
+++ app/_main_components/testimonials_and_statistics.tsx
@@ -4,11 +4,11 @@ import Statistics from './statistics';
 
 export default function TestimonialsAndStatistics() {
     return (
-        <div className="min-h-[calc(100dvh-50px)] h-[calc(100dvh-50px)] w-full flex flex-col justify-evenly">
-            <div className="w-full h-[60%] flex justify-center items-center">   
+        <div className="min-h-[calc(100dvh-50px)] h-[calc(100dvh-50px)] w-full flex flex-col">
+            <div className="w-full h-[60%] sm:h-[70%] md:h-[60%] flex justify-center items-center">   
                 <Testimonials />
             </div>
-            <div className="w-full flex h-[40%]">
+            <div className="w-full flex h-[40%] sm:h-[30%] md:h-[40%]">
                 <Statistics arrows={false}/>
             </div>
         </div>
