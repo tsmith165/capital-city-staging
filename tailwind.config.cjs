2c7f57dec39cc026e6c6418dea163456c1876aca
Torrey Smith <tas.smity@yahoo.com>
2024-06-21 17:45:44 -0700

    Setting 4xl to have 2.75rem line height in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,9 @@ export default {
     theme: {
         extend: {
             colors,
+            fontSize: {
+                '4xl': ['2.25rem', '2.75rem'], // Increase the line-height value as needed
+            },
         },
         listStyleType: {
             none: 'none',
