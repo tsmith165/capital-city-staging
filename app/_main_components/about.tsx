ad8652ea683194c852b011a383e9963cb5e2aa24
Torrey Smith <tas.smity@yahoo.com>
2024-06-24 00:19:53 -0700

    Updated about and contact page

--- app/_main_components/about.tsx
+++ app/_main_components/about.tsx
@@ -4,17 +4,16 @@ import Link from 'next/link';
 
 export default function About() {
     return (
-        <div className="h-auto md:h-[calc(100dvh-50px)] w-full flex flex-row justify-center items-center p-4 pt-0">
-            <div className="relative flex flex-col h-full w-full max-h-full">
+        <div className="h-[calc(100dvh-50px)] w-full flex first-line:justify-center items-center p-4">
+            <div className="relative flex flex-col h-full w-full max-h-full overflow-y-scroll ">
                 <Image
                     src="/bio/bio_pic.jpg"
                     alt="Mia Dofflemyer"
                     width={936}
                     height={1248}
-                    className="rounded-lg shadow-lg w-auto max-h-[60vh] md:max-h-full md:h-[calc(100dvh-50px-2rem)] md:w-fit m-auto md:m-0 lg:ml-[15%]"
+                    className="absolute rounded-lg shadow-lg w-auto h-[calc(45dvh-50px)] md:h-[calc(55dvh-50px)] lg:h-[calc(55dvh-50px)] m-auto left-0 right-0"
                 />
-                <div className="md:absolute md:top-[5%] md:right-0 lg:right-[4%] md:w-1/2 h-fit bg-secondary_dark text-white p-4 space-y-2 rounded-lg shadow-lg">
-                    <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-amber-500 to-yellow-400">
+                <div className="absolute top-[calc(40dvh-50px)] md:top-[calc(50dvh-50px)] lg:top-[calc(50dvh-50px)] w-full h-fit bg-secondary_dark text-white p-4 space-y-2 rounded-lg shadow-lg">                    <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-amber-500 to-yellow-400">
                         Mia Dofflemyer
                     </h1>
                     <p className="text-md md:text-lg">
--- app/contact/contact.tsx
+++ app/contact/contact.tsx
@@ -11,9 +11,9 @@ export default function Contact() {
                     alt="Mia Dofflemyer"
                     width={936}
                     height={1248}
-                    className="rounded-lg shadow-lg w-auto max-h-[35dvh] md:max-h-full md:h-[calc(100dvh-50px-2rem)] md:w-fit m-auto md:m-0 lg:ml-[15%]"
+                    className="absolute rounded-lg shadow-lg w-auto h-[calc(45dvh-50px)] md:h-[calc(55dvh-50px)] lg:h-[calc(55dvh-50px)] m-auto left-0 right-0"
                 />
-                <div className="md:absolute md:top-[5%] md:right-0 xl:w-[60%] md:w-1/2 h-fit bg-secondary_dark text-white p-4 rounded-lg shadow-lg">
+                <div className="absolute top-[calc(40dvh-50px)] md:top-[calc(50dvh-50px)] lg:top-[calc(50dvh-50px)] w-full h-fit bg-secondary_dark text-white p-4 space-y-2 rounded-lg shadow-lg">
                     <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-amber-500 to-yellow-400">
                         Get in Touch with Mia
                     </h1>
