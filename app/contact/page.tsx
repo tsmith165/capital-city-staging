aefc520844d6242363ea808ec30ed103e68de569
Torrey Smith <tas.smity@yahoo.com>
2024-06-25 03:16:09 -0700

    Updated favicon to have background circle

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,9 +13,9 @@ export const metadata: Metadata = {
         'Capital City Staging, Sacramento Staging, Mia Capital City Staging, Mia Realtor, Mia Staging, Staging, Homestaging, Real Estate, Staging Services, Staging Software, Homestaging, Real Estate, Real Estate Staging, Real Estate Staging Services, Real Estate Staging Software, Mia, Mia Dofflemyer, Home Staging, Sacramento, Sacramento Home Staging, Sacramento Real Estate, Sacramento Real Estate Staging, Sacramento Real Estate Staging Services, Sacramento Staging Services, Capital City, Capital, City, City Stage, City Staging, Contact',
     applicationName: 'Capital City Staging',
     icons: {
-        icon: '/logo/CCS_logo.png',
-        shortcut: '/logo/CCS_logo.png',
-        apple: '/ico/apple-touch-icon-152x152.png',
+        icon: '/logo/CCS_logo_w_background.png',
+        shortcut: '/logo/CCS_logo_w_background.png',
+        apple: '/ico/apple-touch-icon-152x152-background.png',
     },
     openGraph: {
         title: 'Capital City Staging',
