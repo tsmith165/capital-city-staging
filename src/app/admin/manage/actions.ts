ac14437364b980cfb457a14a5d5a7e42a0adf805
Torrey Smith <tas.smity@yahoo.com>
2024-07-04 11:56:47 -0700

    Added clerk/uploadthing and updated services

--- src/app/admin/manage/actions.ts
+++ src/app/admin/manage/actions.ts
@@ -1,18 +1,14 @@
 import { eq, asc, desc } from 'drizzle-orm';
-import { db, piecesTable } from '@/db/db';
-import { Pieces } from '@/db/schema';
+import { db, inventoryTable } from '@/db/db';
+import { Inventory } from '@/db/schema';
 import { revalidatePath } from 'next/cache';
 
-export async function getPieces(): Promise<Pieces[]> {
-    return await db.select().from(piecesTable).where(eq(piecesTable.active, true)).orderBy(asc(piecesTable.o_id));
+export async function getInventory(): Promise<Inventory[]> {
+    return await db.select().from(inventoryTable).orderBy(asc(inventoryTable.o_id));
 }
 
-export async function getPrioritizedPieces(): Promise<Pieces[]> {
-    return await db.select().from(piecesTable).where(eq(piecesTable.active, true)).orderBy(desc(piecesTable.p_id));
-}
-
-export async function getDeletedPieces(): Promise<Pieces[]> {
-    return await db.select().from(piecesTable).where(eq(piecesTable.active, false)).orderBy(asc(piecesTable.o_id));
+export async function getDeletedPieces(): Promise<Inventory[]> {
+    return await db.select().from(inventoryTable).orderBy(asc(inventoryTable.o_id));
 }
 
 export async function changeOrder(currIdList: number[], nextIdList: number[]): Promise<void> {
@@ -20,34 +16,8 @@ export async function changeOrder(currIdList: number[], nextIdList: number[]): P
     const [nextId, nextOrderId] = nextIdList;
     console.log(`Swapping ${currId} (${currOrderId}) with ${nextId} (${nextOrderId})`);
 
-    await db.update(piecesTable).set({ o_id: nextOrderId }).where(eq(piecesTable.id, currId));
-    await db.update(piecesTable).set({ o_id: currOrderId }).where(eq(piecesTable.id, nextId));
+    await db.update(inventoryTable).set({ o_id: nextOrderId }).where(eq(inventoryTable.id, currId));
+    await db.update(inventoryTable).set({ o_id: currOrderId }).where(eq(inventoryTable.id, nextId));
 
-    revalidatePath(`/admin/manage`);
-}
-
-export async function changePriority(currIdList: number[], nextIdList: number[]): Promise<void> {
-    const [currId, currPriorityId] = currIdList;
-    const [nextId, nextPriorityId] = nextIdList;
-    console.log(`Swapping priority ${currId} (${currPriorityId}) with ${nextId} (${nextPriorityId})`);
-
-    await db.update(piecesTable).set({ p_id: nextPriorityId }).where(eq(piecesTable.id, currId));
-    await db.update(piecesTable).set({ p_id: currPriorityId }).where(eq(piecesTable.id, nextId));
-    revalidatePath(`/admin/manage`);
-}
-
-export async function setInactive(id: number): Promise<void> {
-    console.log(`Setting piece with id: ${id} as inactive`);
-    await db.update(piecesTable).set({ active: false, o_id: -1000000 }).where(eq(piecesTable.id, id));
-    revalidatePath(`/admin/manage`);
-}
-
-export async function setActive(id: number): Promise<void> {
-    console.log(`Setting piece with id: ${id} as active`);
-
-    const lastPiece = await db.select().from(piecesTable).orderBy(desc(piecesTable.o_id)).limit(1);
-    const newOId = lastPiece.length > 0 ? lastPiece[0].o_id + 1 : 1;
-
-    await db.update(piecesTable).set({ active: true, o_id: newOId }).where(eq(piecesTable.id, id));
     revalidatePath(`/admin/manage`);
 }
