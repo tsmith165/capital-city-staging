ad09446b9e6c61690c0544446a16b9af9ae727ce
Torrey Smith <tas.smity@yahoo.com>
2024-07-05 22:14:01 -0700

    Added inventory page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,33 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+import { isClerkUserIdAdmin } from '@/utils/auth/ClerkUtils';
 
-const isPrivateRoute = createRouteMatcher(['/admin(.*)', '/admin/inventory(.*)', '/admin/tools(.*)']);
+const isPrivateRoute = createRouteMatcher(['/admin/inventory', '/admin/tools', '/admin/manage']);
 
 export default clerkMiddleware(async (auth, req) => {
     const route_is_private = isPrivateRoute(req);
     console.log('Current route: ' + req.url);
     console.log('Route is private? ' + route_is_private);
 
-    if (route_is_private === true) {
+    if (route_is_private === true) {    
         console.log('Route is private.  Protecting with admin role.');
-        try {
-            auth().protect(has => {
-                return (
-                  has({ role: 'org:admin' })
-                )
-              })
-        } catch (error) {
-            console.error('Error protecting route with admin role:', error);
+        console.log('Current auth object: ' + JSON.stringify(auth()));
+
+        const userId = auth().userId;
+        console.log('User ID: ' + userId);
+
+        if (!userId) {
+            console.log('User ID is null. Redirecting to home page.');
             return NextResponse.redirect(new URL('/', req.url));
         }
+
+        const hasAdminRole = await isClerkUserIdAdmin(userId);
+        console.log('User hasAdminRole: ' + hasAdminRole);
+        if (!hasAdminRole) {
+            console.log('User does not have admin role. Redirecting to home page.');
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+        console.log('User has admin role. Continuing...');
     }
     return NextResponse.next();
 });
